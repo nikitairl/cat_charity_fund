ebb2dde291747addd71f143acdc820c4eb522a27
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-04-04 23:43:14 +0300

    pep

--- app/core/init_db.py
+++ app/core/init_db.py
@@ -30,12 +30,10 @@ async def create_user(email: EmailStr, password: str, is_superuser: bool = False
 
 
 async def create_first_superuser():
-    if (
-        settings.first_superuser_email is not None
-        and settings.first_superuser_password is not None
-    ):
-        await create_user(
-            email=settings.first_superuser_email,
-            password=settings.first_superuser_password,
-            is_superuser=True,
-        )
+    if settings.first_superuser_email is not None:
+        if settings.first_superuser_password is not None:
+            await create_user(
+                email=settings.first_superuser_email,
+                password=settings.first_superuser_password,
+                is_superuser=True,
+            )
